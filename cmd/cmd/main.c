6bf043faf0f9977d5dbb9d3b6f48a0fa2ec51610
Simon Wunderlin <swunderlin@gmail.com>
2014-08-28 20:05:41 +0200

    added

---
+++ cmd/cmd/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  cmd
+//
+//  Created by Simon Wunderlin on 28/08/14.
+//  Copyright (c) 2014 ___HACKERSINC___. All rights reserved.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	CFShow(CFSTR("Hello, World!\n"));
+	return 0;
+}
+
